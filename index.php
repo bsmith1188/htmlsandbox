8c0af8314749c4a4e5007ae103d9745028c91c6c
Ben Smith <rios7467@gmail.com>
2017-01-10 15:50:20 -0700

    Set head and bones for html.

--- index.php
+++ index.php
@@ -4,5 +4,36 @@
 		<meta charset="utf-8" />
 		<title>HTML SANDBOX</title>
 	</head>
-	<body></body>
+	<body>
+		<h1>This is a demo page.</h1>
+		<p>Dummy text. Dummy text. Dummy text. Dummy text. Dummy text. Dummy text. Dummy text. Dummy text. Dummy text. Dummy text. Dummy text. Dummy text. Dummy text. Dummy text. Dummy text. Dummy text. Dummy text. Dummy text. Dummy text. Dummy text. Dummy text.</p>
+		<h2>This is a list.</h2>
+		<ul>
+			<li>Lsit item 1.</li>
+			<li>List item 2.</li>
+			<li>List item 3.</li>
+			<!-- this is a comment -->
+		</ul>
+		<h3>This is a table. Column 1 is cats and Column 2 is dogs.</h3>
+		<table>
+			<tr>
+				<th>Column 1</th>
+				<th>Column 2</th>
+			</tr>
+			<tr>
+				<td>Tabby</td>
+				<td>Beagle</td>
+			</tr>
+			<tr>
+				<td>Persian</td>
+				<td>Bulldog</td>
+			</tr>
+			<tr>
+				<td>Siamese</td>
+				<td>Boxer</td>
+			</tr>
+		</table>
+		<img src="images/catdog.png" alt="catdog photo">
+		<p>This is a link to <a href="https://www.google.com" target="_blank">google</a>.</p>
+	</body>
 </html>
